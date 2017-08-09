ceda438286a72a32934ba182f799f2ef143300cf
Balachandar.p <balachandartamilan@gmail.com>
2017-08-09 15:58:00 +0530

    Create checkno.c

---
+++ checkno.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+{
+printf("Number is positive");
+}else if(a==0)
+{
+printf("number is Zero");
+}
+else
+{
+printf("Number is negative");
+}
+return 0;}
